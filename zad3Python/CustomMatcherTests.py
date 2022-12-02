26159966baae56567155a999746188304dd3e31a
warhir <krzyzopolski.mariusz@gmail.com>
2022-12-02 14:02:55 +0100

    zad 3 pycrest CustomMatchers

---
+++ zad3Python/CustomMatcherTests.py
@@ -0,0 +1,29 @@
+import unittest
+
+from hamcrest import assert_that, is_, is_not
+
+from zad3Python.CustomMatcher import on_first_change, minimal_brutto_pay
+from zad3Python.ProductionWorker import ProductionWorker
+
+
+class ProductionWorkerTest(unittest.TestCase):
+    def test_worker_on_first_change(self):
+        worker = ProductionWorker()
+        worker.set_change_number(1)
+        assert_that(worker, is_(on_first_change()))
+
+    def test_worker_on_second_change(self):
+        worker = ProductionWorker()
+        worker.set_change_number(2)
+        assert_that(worker, is_not(on_first_change()))
+
+    def test_worker_on_illegal_pay(self):
+        worker = ProductionWorker()
+        worker.set_pay_hour(11)
+        assert_that(worker, is_not(minimal_brutto_pay()))
+
+    def test_worker_on_legal_pay(self):
+        worker = ProductionWorker()
+        worker.set_pay_hour(20)
+        assert_that(worker, is_(minimal_brutto_pay()))
+
