5b245f3ef84572a342f0529a15f704d9507ec247
warhir <krzyzopolski.mariusz@gmail.com>
2022-12-02 20:02:36 +0100

    zad 3 java

--- zad3/src/main/java/org/example/Fraction.java
+++ zad3/src/main/java/org/example/Fraction.java
@@ -1,7 +1,8 @@
+package org.example;
+
 import com.google.common.annotations.VisibleForTesting;
 
 import java.io.Serializable;
-
 
 public final class Fraction implements Serializable {
     public static final Fraction of(final int nominator, final int denominator) {
@@ -15,6 +16,7 @@ public final class Fraction implements Serializable {
                 case 1:
                     return FULL;
                 case 2:
+                    System.out.println(HALF);
                     return HALF;
                 case 3:
                     return ONE_THIRD;
@@ -35,18 +37,41 @@ public final class Fraction implements Serializable {
     public boolean equals(final Object rightSide){
         if(!(rightSide instanceof Fraction)) return false;
         final Fraction that = (Fraction) rightSide;
+        System.out.println((this == that) || (nominator == that.nominator && denominator == that.denominator));
+
         return (this == that) || (nominator == that.nominator && denominator == that.denominator);
+    }
+    public Fraction add(final Object firstFraction, final Object secondFraction){
+        int first,second;
+        first=((((Fraction) firstFraction).nominator * ((Fraction) secondFraction).denominator) + (((Fraction) secondFraction).nominator * ((Fraction) firstFraction).denominator));
+        second=((Fraction) firstFraction).denominator * ((Fraction) secondFraction).denominator;
+        return of(first,second);
+    }
+    public Fraction multiply(final Object firstFraction, final Object secondFraction){
+        int first,second;
+        first= ((Fraction) firstFraction).nominator * ((Fraction) secondFraction).nominator;
+        second= ((Fraction) firstFraction).denominator * ((Fraction) secondFraction).denominator;
+        return of(first,second);
     }
     @Override
     public int hashCode(){
         return nominator + denominator;
     }
+    public static int connect(int first_element, int second_element) {
+        if(second_element == 0){
+            return first_element;
+        }
+        else {
+            return connect(second_element, first_element % second_element);
+        }
+    }
     @Override
     public String toString(){
+        int connector = connect(nominator, denominator);
         return new StringBuilder()
-                .append(nominator)
+                .append(nominator/ connector)
                 .append('/')
-                .append(denominator)
+                .append(denominator / connector)
                 .toString();
     }
     Fraction(final int nominator, final int denominator){
---
+++ zad3/src/test/java/org/example/TestFractionHamcrest.java
@@ -0,0 +1,52 @@
+package org.example;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+public class TestFractionHamcrest {
+    @Test
+    public void checkIfFractionsEqual() {
+        Fraction firstFraction = new Fraction(2, 3);
+        Fraction secondFraction = new Fraction(2, 3);
+        assertThat(firstFraction.equals(secondFraction), equalTo(true));
+    }
+
+    @Test
+    public void checkFractionsOneHalfFractionOfEquals() {
+        Fraction firstFraction = new Fraction(2, 1);
+        assertThat(firstFraction.of(2, 1), is(equalTo(new Fraction(1, 2))));
+    }
+
+    @Test
+    public void checkFractionsOneThirdFractionIs() {
+        Fraction firstFraction = new Fraction(3, 1);
+        assertThat(firstFraction.of(3, 1), is(new Fraction(1, 3)));
+    }
+    @Test
+    public void checkFractionsMultiply() {
+        Fraction firstFraction = new Fraction(1, 2);
+        Fraction secondFraction = new Fraction(1, 2);
+        assertThat(firstFraction.multiply(firstFraction, secondFraction).toString(), is("1/4"));
+    }
+
+    @Test
+    public void checkFractionsNegativeMultiply() {
+        Fraction firstFraction = new Fraction(-11, 20);
+        Fraction secondFraction = new Fraction(3, 7);
+        assertThat(firstFraction.multiply(firstFraction, secondFraction).toString(), is("33/-140"));
+    }
+
+    @Test
+    public void checkFractionsAdd() {
+        Fraction firstFraction = new Fraction(5, 26);
+        Fraction secondFraction = new Fraction(18, 210);
+        assertThat(firstFraction.add(firstFraction, secondFraction).toString(), equalToIgnoringCase("253/910"));
+    }
+
+    @Test
+    public void checkFractionsNegativeAdd() {
+        Fraction firstFraction = new Fraction(20, 120);
+        Fraction secondFraction = new Fraction(-50, 69);
+        assertThat(firstFraction.add(firstFraction, secondFraction).toString(), equalToIgnoringCase("-77/138"));
+    }
+}
